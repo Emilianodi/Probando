baa445724c1657c276e44fe13ec75f3391bdde86
Emiliano <r.diazemiliano.red@gmail.com>
2023-05-06 13:03:38 -0300

    Agregar archivos de proyecto.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    //
+    //Get: /HelloWorld/
+    /*public string Index()
+    {
+        return "Hola, voy a devolver una cadena de texto";
+    }*/
+    public IActionResult Index()
+    { 
+        return View();
+    }
+    //
+    //Get:/HelloWorld/Welcome/
+
+    /*public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }*/
+
+    //Requires using System.Text.Encodings.Web;
+
+    /*public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is {numTimes}");
+    }*/
+
+    /*public string Welcome(string name, int ID =1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }*/
+
+    public IActionResult Welcome(string name, int numTimes=1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+
+}
